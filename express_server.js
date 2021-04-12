7b4481c6ffa1b726510980b6d1c8d040e2cdd4fa
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-04-12 14:50:10 -0400

    Add a POST Route to Receive the form submission and add generateRandomString function that returns a string of 6 random alphanumeric characters

--- express_server.js
+++ express_server.js
@@ -1,21 +1,23 @@
 const PORT = 8080;
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.set("view engine", "ejs");
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// Generates a "unique" shortURL [0-9]
+const generateRandomString = function() {
+  return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
+};
+
 app.get("/", (req, res) => {
   res.send("Hello!");
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 // Display basic form page that allows user to submit URLs to be shortened
@@ -35,10 +37,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+// Handle the form submission
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`App listening on port ${PORT}!`);
 });
