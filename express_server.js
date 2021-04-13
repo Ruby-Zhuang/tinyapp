8e484bde3df6539c658d4cdb7dc1ec5b8a984f0e
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-04-13 13:38:54 -0400

    Fix bug in update route from urls_index to urls_show. Update generrate randomString function to generate numbers, letters (uppercase and lowercase).

--- express_server.js
+++ express_server.js
@@ -24,65 +24,98 @@ const urlDatabase = {
 
 // Generates a new "unique" shortURL [0-9 a-z A-Z]
 const generateRandomString = (numCharacters) => {
-  const randomString = Math.random().toString(36).substring(2, numCharacters + 2);
+  //const randomString = Math.random().toString(36).substring(2, numCharacters + 2);
+  const stringList = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let randomString = '';
+  for (let i = 1; i <= numCharacters; i++) {
+    let randomCharIndex = Math.floor(Math.random() * stringList.length);
+    let randomChar = stringList[randomCharIndex];
+    randomString += randomChar;
+  }
+
+  // Recursive case if randomString already exists as a shortURL in the database
+  if (urlDatabase[randomString]) {
+    generateRandomString(numCharacters);
+  }
+
   return randomString;
 };
+
+// Possible helper functions to improve readiblility
+// addNewURL
+// updateURL
 
 /////////////////////////////////////////////////////////////
 // GET REQUESTS ---------------------------------------------
 /////////////////////////////////////////////////////////////
 
-// Home route
-app.get("/", (req, res) => {
-  res.send("Hello!");
+// READ: Redirect any request to "/u/:shortURL" to its longURL
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
 });
 
-// Display basic form page that allows user to submit URLs to be shortened
+// READ: Display basic form page that allows user to submit URLs to be shortened
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-// Display a single URL and its shortened form
+// READ: Display the form submission to update a specific existing shortened URL in database
+// app.get("/urls/:shortURL/update", (req, res) => {
+//   const shortURL = req.params.shortURL;
+//   res.redirect(`/urls/${shortURL}`);
+// });
+
+// READ: Display a single URL and its shortened form along with a form to update a specific existing shortened URL in database
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[req.params.shortURL];
+  const templateVars = { shortURL, longURL };
   res.render("urls_show", templateVars);
 });
 
-// Display all the URLs and their shortened forms
+// READ: Display all the URLs and their shortened forms
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-// Redirect any request to "/u/:shortURL" to its longURL
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+// READ: Redirect to display all the URLs if user goes to root
+app.get("/", (req, res) => {
+  res.redirect(`/urls`);
+});
+
+// READ: For development purposes - JSON string representing the entire urlDatabase object
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 /////////////////////////////////////////////////////////////
 // POST REQUESTS --------------------------------------------
 /////////////////////////////////////////////////////////////
 
-// Handle the form submission to remove existing shortened URLs from database
-app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL];
+// CREATE/POST: Handle the form submission to add the long URL to the database with an associated random short URL
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString(6);
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+// UPDATE/PUT: Handle the update request from the home page
+app.post("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
   res.redirect(`/urls`);
 });
 
-// Handle the form submission to update existing shortened URLs in database
-app.post("/urls/:shortURL", (req, res) => {
-  urlDatabase[req.params.shortURL] = req.body.longURL;
-  res.redirect(`/urls/${req.params.shortURL}`);
-});
-
-// Handle the form submission to add the long URL to the database with an associated random short URL
-app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString(6);
-  urlDatabase[shortURL] = req.body.longURL;
-  // const templateVars = { shortURL, longURL: urlDatabase[shortURL] };
-  // res.render("urls_show", templateVars);
-  res.redirect(`/urls/${shortURL}`);
+// DELETE/DELETE: Handle the form submission to remove a specific existing shortened URL from database
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect(`/urls`);
 });
 
 /////////////////////////////////////////////////////////////
