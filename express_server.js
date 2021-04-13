24bf3a4abbeeb1014f67150be49e3f57ade8deb8
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-04-13 17:57:30 -0400

    Bold username in navbar to improve readibility

--- express_server.js
+++ express_server.js
@@ -12,8 +12,20 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
 /////////////////////////////////////////////////////////////
-// DATABASE -------------------------------------------------
+// DATABASES -------------------------------------------------
 /////////////////////////////////////////////////////////////
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
