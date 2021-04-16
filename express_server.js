1044e9b8617beae54addde08f01f7ef0cacdf3f1
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-04-15 21:53:50 -0400

    Create README file and update package.json with author and description. Upload screenshots.

--- express_server.js
+++ express_server.js
@@ -47,15 +47,15 @@ const users = {
 const urlDatabase = {
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
-    userID: "aJ48lW"
+    userID: "userRandomID"
   },
   i3BoGr: {
     longURL: "https://www.google.ca",
-    userID: "aJ48lW"
+    userID: "userRandomID"
   },
   b2xVn2: {
     longURL: "http://www.netflix.com",
-    userID: "userRandomID"
+    userID: "aJ48lW"
   },
   sgq3y6: {
     longURL: "http://www.lighthouselabs.ca",
